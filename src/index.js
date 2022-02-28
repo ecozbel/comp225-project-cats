fd368b5b4b7e404d0c045c27d16bf6659b1c4ffd
ecozbel <73610579+ecozbel@users.noreply.github.com>
2022-02-28 15:12:38 -0600

    -Initial Concept test
    -Draggable sprites
    -Hitboxes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
 import logoImg from './assets/logo.png';
-
+import catimg from './assets/cat.png';
+import hatimg from './assets/hat.png';
 class MyGame extends Phaser.Scene
 {
     constructor ()
@@ -11,28 +12,58 @@ class MyGame extends Phaser.Scene
     preload ()
     {
         this.load.image('logo', logoImg);
+        this.load.image('cat',catimg);
+        this.load.image('hat',hatimg);
+        this.load.json('catshape', 'assets/cat-shape2.json');
     }
       
     create ()
     {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
+        var catshape = this.cache.json.get('catshape');
+        // this.arcade.world.setBounds(0, 0, game.config.width, game.config.hei
+        // this.arcade.world.setGravity(0,0);
+        var cat = this.physics.add.sprite(400,300,'cat',null, { shape: catshape});
+        var hat = this.physics.add.sprite(100,300,'hat');
+        cat.setScale(0.6);
+        hat.setScale(0.6);
+        hat.setInteractive();
+        this.input.setDraggable(hat);
+        this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+
+            gameObject.x = dragX;
+            gameObject.y = dragY;
+
+    
         });
+         while(checkOverlap(hat,cat))
+        {
+            hat.setPosition(cat.getCenter);
+            console.log("Collision!");
+        }
+        function checkOverlap(spriteA, spriteB) {
+            var boundsA = spriteA.getBounds();
+            var boundsB = spriteB.getBounds();
+            console.log(boundsA);
+            console.log(boundsB);
+            return Phaser.Geom.Intersects.RectangleToRectangle(boundsA, boundsB);
+            }
     }
 }
+
 
 const config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
+    pixelArt:true,
     width: 800,
     height: 600,
+    backgroundColor: '#FFFFFF',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
     scene: MyGame
 };
 
