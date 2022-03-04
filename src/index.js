657630edb5738c3c169cbae38c136b5403786bbd
ecozbel <73610579+ecozbel@users.noreply.github.com>
2022-03-04 00:21:57 -0600

    -Added testing for closet categories

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import hatimg from './assets/hat.png';
 import catHitbox from './assets/cat-shape2.json';
 var cat;
 var hat;
+var clothingType;
+var blankSprite;
 var clothingTypes;
 
 class MyGame extends Phaser.Scene
@@ -32,7 +34,14 @@ class MyGame extends Phaser.Scene
         cat = this.matter.add.sprite(400,300,'cat',null, { shape: catshape});
         hat = this.matter.add.sprite(600,300,'hat');
 
+        blankSprite = this.matter.add.sprite(600,300,'hat');
+        blankSprite.setVisible(false);
+        blankSprite.setSensor(true);
+        blankSprite.setInteractive(false);
 
+        cat.setData('catLayer',this.add.layer());
+
+        //var layers = Phaser.Utils.Array();
         
 
         //placeholder for shoes
@@ -42,8 +51,8 @@ class MyGame extends Phaser.Scene
 
 
         createClothingSnapPoints(cat);
-
-        clothingTypes = {//did this because I dont think javascript has enums
+        //did this because I dont think javascript has enums
+        clothingTypes = {
 
             hat : 0,
             shoe : 1,
@@ -59,21 +68,23 @@ class MyGame extends Phaser.Scene
 
         shoe.setScale(0.2);
         shoe.setInteractive();
+        shoe.setSensor(true);
+
         shoe.clothingType = clothingTypes.shoe;
         hat.clothingType = clothingTypes.hat;
         
         this.input.setDraggable(hat);
         this.input.setDraggable(shoe);
 
-        //Creates a layer acting as a closet. Layer is like a type of array, but meant to store graphics objects.
+        //Creates a layer acting as a closet category. Layer is like a type of array, but meant to store graphics objects.
         var hatGroup = this.add.layer();
-        //createCloset(hatGroup);
-
         //Adds hat to closet
         hatGroup.add(hat);
-        hatGroup.add(shoe);
 
-        //Organizes items in layer in a grid
+        var shoeGroup = this.add.layer();
+        shoeGroup.add(shoe);
+
+        //Organizes items in layer in a grid. For testing purposes, hasn't been encapsulated yet.
         Phaser.Actions.GridAlign(hatGroup.getChildren(), {
                     width: 3,
                     height: 10,
@@ -83,11 +94,67 @@ class MyGame extends Phaser.Scene
                     y: 50
                 });
 
-        //Goes through each sprite in the layer (closet), and saves their origin position
+        Phaser.Actions.GridAlign(shoeGroup.getChildren(), {
+                    width: 3,
+                    height: 10,
+                    cellWidth: 50,
+                    cellHeight: 50,
+                    x: 50,
+                    y: 50
+                });
+
+
+        //Goes through each sprite in the hat group and saves their origin position and index
+        //Also saves what group they belong to
         //Needed for snapping back/un-equipping
         hatGroup.each(function(gameObject) {
             gameObject.setData('origin', gameObject.getCenter());
+            gameObject.setData('group', hatGroup);
+            gameObject.setData('index', hatGroup.getIndex(gameObject));
         });
+
+        shoeGroup.each(function(gameObject) {
+            gameObject.setData('origin', gameObject.getCenter());
+            gameObject.setData('group', shoeGroup);
+            gameObject.setData('index', shoeGroup.getIndex(gameObject));
+        });
+
+        //only show hats first until toggle is pressed
+        shoeGroup.setVisible(false);
+        var layers = [shoeGroup,hatGroup];
+
+    
+
+        //Test button for switching between categories
+        const clickButton = this.add.text(40, 100, 'Toggle!', { fill: '#0f0' })
+        .setInteractive()
+        .on('pointerdown', () => toggleVisible(layers));
+
+        //
+        var self = this;
+
+        //temporary function to emulate switching categories
+        function toggleVisible(layers){
+            for(const layer  of layers){
+                if(layer.visible==true){
+                    layer.setVisible(false);
+                    layer.each(function(gameObject) {
+                        if(gameObject != null){
+                            self.input.setDraggable(gameObject,false);
+                        }
+                    });
+                }
+                else{
+                    layer.setVisible(true);
+                    layer.each(function(gameObject) {
+                        if(gameObject != null){
+                            self.input.setDraggable(gameObject,true);
+                        }
+                    });
+                }
+            }
+            return;
+        }
 
 
         //different clothes snap to different places on cat. only shoe and hat right now
@@ -110,6 +177,7 @@ class MyGame extends Phaser.Scene
 
     update(){
         this.input.on('drag', (pointer, gameObject, dragX, dragY) => {
+            gameObject.addToDispla
             gameObject.setScale(0.6);
             gameObject.x = dragX;
             gameObject.y = dragY;
@@ -123,7 +191,8 @@ class MyGame extends Phaser.Scene
         //overlap check and snap
         function snapToCat(sprite, pointer) {
             if(Phaser.Geom.Intersects.RectangleToRectangle(sprite.getBounds(), cat.getBounds())){
-            
+                sprite.getData('group').replace(sprite,blankSprite );
+                cat.getData('catLayer').add(sprite);
 
                 //switch statement to handle multiple types of clothes
                 switch (sprite.clothingType){
@@ -141,9 +210,11 @@ class MyGame extends Phaser.Scene
             }
             //Sprite shrinks and returns to closet if it is not dropped on cat.
             else{
+                sprite.getData('group').addAt(sprite, sprite.getData('index'));
                 sprite.setScale(0.2);
                 sprite.x=sprite.getData('origin').x;
                 sprite.y=sprite.getData('origin').y;
+                
             }
         }
     }
@@ -160,7 +231,7 @@ const config = {
     physics: {
         default: 'matter',
         matter: {
-            debug: true
+            debug: false
         }
     },
     scene: MyGame
