f61ef0cf3e9d00287f2b3b92bf2d6d8a1bf0a13b
NawalChaudhry <70664934+NawalChaudhry@users.noreply.github.com>
2022-03-08 22:00:25 -0600

    uploaded and imported flower top

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import catHitbox from './assets/cat-shape2.json';
 import shoe1img from './assets/shoe1.png';
 import closetimg from './assets/closet.png';
 import greenshirt from './assets/greenshirt.png';
+import flowertop from './assets/flowertop.png';
 
 var cat;
 var closet;
