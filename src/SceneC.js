7c085761ea0d0d47361f9aeab0bade474c6b7ba2
Jarett Baker-Dunn <72581853+mintLine@users.noreply.github.com>
2022-03-31 23:03:42 -0500

    cat draws in sceneC

--- src/SceneC.js
+++ src/SceneC.js
@@ -1,6 +1,7 @@
 import Phaser from "phaser";
 import { game } from './index.js';
 
+
 class EndingScene extends Phaser.Scene
 {
     constructor ()
@@ -18,7 +19,23 @@ class EndingScene extends Phaser.Scene
     {
         console.log("ending scene cat: " + this.game.cat);
         console.log(this.game.cat);
+
+        this.setUpCat(400,300);
+
     }
+
+    setUpCat(_x,_y){
+
+        var cat = this.add.existing(this.game.cat);
+        cat.x=_x;
+        cat.y=_y;
+
+
+    }
+
+
+
+
 }
 
 
