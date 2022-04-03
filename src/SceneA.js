059a61966f610c0837b836f355d91dcb3775290d
ecozbel <73610579+ecozbel@users.noreply.github.com>
2022-04-03 16:11:40 -0500

    -fixed indent

--- src/SceneA.js
+++ src/SceneA.js
@@ -264,21 +264,21 @@ class BegginingScene extends Phaser.Scene
     }
         
 //Utilities
-        //Scales given sprite to normal size
-        function normalizeScale(sprite){
-            sprite.displayWidth=game.config.width*0.3; 
-            sprite.scaleY=sprite.scaleX;
-        }
-        //Scales given sprite to clothing size
-        function normalizeClothing(sprite){
-            sprite.displayWidth=game.config.width*0.28; 
-            sprite.scaleY=sprite.scaleX;
-        }
-        //Scales given sprite to icon size
-        function scaletoIconSize(sprite){
-            sprite.displayWidth=game.config.width*0.08; 
-            sprite.scaleY=sprite.scaleX;
-        }
+//Scales given sprite to normal size
+function normalizeScale(sprite){
+    sprite.displayWidth=game.config.width*0.3; 
+    sprite.scaleY=sprite.scaleX;
+}
+//Scales given sprite to clothing size
+function normalizeClothing(sprite){
+    sprite.displayWidth=game.config.width*0.28; 
+    sprite.scaleY=sprite.scaleX;
+}
+//Scales given sprite to icon size
+function scaletoIconSize(sprite){
+    sprite.displayWidth=game.config.width*0.08; 
+    sprite.scaleY=sprite.scaleX;
+}
 
 function createPalettes(catRandomizerConfig,game)
 {
