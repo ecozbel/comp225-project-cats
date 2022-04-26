6274c4761ca67f278029295c5e996d9c771a9fec
ecozbel <73610579+ecozbel@users.noreply.github.com>
2022-04-25 20:50:42 -0500

    -gallery should work even without any games finished

--- src/galleryScene.js
+++ src/galleryScene.js
@@ -51,31 +51,9 @@ class sceneGallery extends Phaser.Scene
                     cat.setVisible(false);
 
                     hatKey = localStorage.getItem('hat'+i);
-                    //console.log(hatKey);
-                    // if(hatKey != 'empty'&&hatKey !=null){
-                    //     hat = self.add.sprite(0,0,hatKey).setScale(0.3)
-                    //     hat.setVisible(false);
-                    // }
-
                     shirtKey = localStorage.getItem('shirt'+i);
-                    //console.log(hatKey);
-                    // if(shirtKey != 'empty'&&shirtKey !=null){
-                    //     shirt = self.add.sprite(0,0,shirtKey).setScale(0.3)
-                    //     shirt.setVisible(false);
-                    // }
                     pantsKey = localStorage.getItem('pants'+i);
-                    //console.log(hatKey);
-                    // if(pantsKey != 'empty'&&pantsKey !=null){
-                    //     pants = self.add.sprite(0,0,pantsKey).setScale(0.3)
-                    //     pants.setVisible(false);
-                    // }
                     shoesKey = localStorage.getItem('shoes'+i);
-                    //console.log(hatKey);
-                    // if(shoesKey != 'empty'&&shoesKey !=null){
-                    //     shoes = self.add.sprite(0,0,shoesKey).setScale(0.3)
-                    //     shoes.setVisible(false);
-                    // }
-                    //console.log(hatKey)
                     var chosenHat = addClothing(hatKey,hat,self)
                     var chosenShirt = addClothing(shirtKey,shirt,self)
                     var chosenPants= addClothing(pantsKey,pants,self)
@@ -93,7 +71,6 @@ class sceneGallery extends Phaser.Scene
                         flip: false
                     })
                     card.setScale(0.5);
-                    //polaroids.push(card)
                     card.name = localStorage.getItem('catName'+i)
                     polaroids.push(card)
                 }
@@ -104,35 +81,46 @@ class sceneGallery extends Phaser.Scene
                         flip: false
                     })
                     card.setScale(0.5);
-                    //polaroids.push(card)
                     card.name = localStorage.getItem('catName'+i)
-                    //polaroids.push(card)
-                    // card.setInteractive(true);
-                    // card.on('pointerover', () => card.setScale(0.8))
-                    // card.on('pointerout', () => card.setScale(0.5))
                     polaroids.push(card)
 
                 }
 
             }
+        }
+        if (polaroids.length !=0 && polaroidCount!=null){
+            //Carousel (webGL only): a display for polaroids
             var carousel = this.add.rexPerspectiveCarousel({
                 x: 400, y: 280,
                 z:1,
                 faces: polaroids,
-                //faceSpace: 150,
                 faceWidth: 400,
-                //backWidth: 400
+            })         
+        }
+        else{
+            for(let i=0;i<10;i++){
+                let emptyCard = self.add.rexPerspectiveCard({
+                    front: { key: 'polaroidBack' },
+                    back: { key: 'polaroidBack'},
+                    flip: false
+                })
+                emptyCard.setScale(0.5);
+                polaroids.push(emptyCard)
+            }
+                //Carousel (webGL only): a display for polaroids
+            var carousel = this.add.rexPerspectiveCarousel({
+                x: 400, y: 280,
+                z:1,
+                faces: polaroids,
+                faceWidth: 400,
             })
 
-            //var invisRect = this.add.rectangle(400,280,400,400)
-            var r1 = this.add.graphics();
-            r1.setInteractive(polaroids[carousel.face].getBounds(), Phaser.Geom.Rectangle.Contains);
-            //invisRect.setVisible(false);
-            r1.on('pointerover', () => polaroids[carousel.face].setScale(1))
-            r1.on('pointerout', () => polaroids[carousel.face].setScale(0.5))
-            
         }
-        
+            //Invisible rectangle to act as hit area
+        var r1 = this.add.graphics();
+        r1.setInteractive(polaroids[carousel.face].getBounds(), Phaser.Geom.Rectangle.Contains);
+        r1.on('pointerover', () => polaroids[carousel.face].setScale(1))
+        r1.on('pointerout', () => polaroids[carousel.face].setScale(0.5))
 
         function addClothing(key,clothing,currentScene){
             // console.log(clothing)
@@ -140,40 +128,30 @@ class sceneGallery extends Phaser.Scene
                 clothing = currentScene.add.sprite(0,0,key).setScale(0.3)
                 clothing.setVisible(false);
                 return clothing;
-                //console.log(clothing)
-                //return(clothing)
             }
             if(key == 'empty'|| key==null){
                 clothing = null;
                 return clothing;
-                //clothing.setVisible(false);
             }
         }
-
-
         function addPolaroid(key,givenHat,givenShirt,givenPants,givenShoes,bg) {
             let polaroid = self.add.renderTexture(800, 600, 800, 600);
-            
             polaroid.setVisible(false);
             polaroid.setOrigin(0.5)
 
-           //draw black square
+           //draw black square (frame background)
            var r2 = self.add.rectangle(0, 0, 300, 300, 0x000000);
            r2.setVisible(false);
            r2.setDepth(-1);
-            //r2.setStrokeStyle(4, 0x000000);
-           
+           //draw the background and frame elements
            polaroid.draw(r2, 400,300 );
            polaroid.draw(bg, 400,300 );
            polaroid.draw(frame, 400,300 );
 
-           //draw the background
-     
             // draw the cat
             polaroid.draw(cat, 400,297 );
      
             // draw the clothes
-            
             if(givenShoes!=null){
                 polaroid.draw(givenShoes, 406, 372);
             }
@@ -186,15 +164,10 @@ class sceneGallery extends Phaser.Scene
             if(givenHat!=null){
                 polaroid.draw(givenHat, 405, 217);
             }
-            //polaroid.draw(hat, 400, 130);
-            //console.log(self.shirt)
-            //console.log(hat)
+            //Save as texture
             polaroid.saveTexture(key);
 
         }
-        
-
-
         function getPromptWithName(prompt,name) {
             return {
                 objective: prompt.objective.replaceAll("{{full_name}}", name),
@@ -202,7 +175,7 @@ class sceneGallery extends Phaser.Scene
                 outcome: prompt.outcome.replaceAll("{{full_name}}", name)
             };
         }
-
+        //Put the clothes into the scene
         function setUpClothes(currentScene){
     
             if (hatKey != null) {
@@ -218,8 +191,7 @@ class sceneGallery extends Phaser.Scene
                 pants = currentScene.add.sprite(0,0,pantsKey);
             }
         }
-        // console.log(carousel.face);
-
+        //Button to back out to menu
         var backButton= new utilities.genericButton({scene:self,key:'buttonFrame',x:80,y:50,text:"Back"});
         backButton.setDisplaySize(100,54);
         backButton.on('pointerdown', function(pointer, localX, localY, event){
@@ -228,6 +200,7 @@ class sceneGallery extends Phaser.Scene
         function startPreviousScene(){
             self.scene.start('sceneA_mainMenu');
         }
+        //Button to go left on carousel
         const leftButton = this.add.image(80, 300, 'leftButton',0).setInteractive().setDepth(4);
         leftButton.on('pointerover', () => leftButton.setFrame(1))
         leftButton.on('pointerout', () => leftButton.setFrame(0))
@@ -235,10 +208,14 @@ class sceneGallery extends Phaser.Scene
             carousel.roll.toLeft(300);  
             console.log(carousel.face); 
             let catName = polaroids[carousel.face].name
-            infoText.setText('Cat name: ' +catName);
+            if (catName !=null){
+                infoText.setText('Cat name: ' +catName);
+            }
+            else{
+                infoText.setText('Cat name: TBD - Play to create memories!');
+            }
         },self );
-        
-
+        //Button to go right on carousel
         const rightButton = this.add.image(720, 300, 'rightButton',0).setInteractive().setDepth(4);
         rightButton.on('pointerover', () => rightButton.setFrame(1))
         rightButton.on('pointerout', () => rightButton.setFrame(0))
@@ -246,13 +223,23 @@ class sceneGallery extends Phaser.Scene
             carousel.roll.toRight(300); 
             console.log(carousel.face);
             let catName = polaroids[carousel.face].name
-            infoText.setText('Cat name: ' +catName);
+            if (catName !=""){
+                infoText.setText('Cat name: ' +catName);
+            }  
+            else{
+                infoText.setText('Cat name: TBD - Play to create memories!');
+            }
         },self );
-
+        //Name to display at render
         var firstName = polaroids[carousel.face].name
-        const infoText = this.add.text(250, 450, 'Cat name: ' + firstName).setDepth(-1);
-
-
+        if (firstName !=""){
+            var infoText = this.add.text(250, 450, 'Cat name: ' + firstName).setDepth(-1);
+        }  
+        else{
+            var infoText = this.add.text(250, 450, 'Cat name: TBD - Play to create memories!').setDepth(-1);
+        }
+        //const infoText = this.add.text(250, 450, 'Cat name: ' + firstName).setDepth(-1);
+        //Button to download current displayed polaroid as png
         var downloadButton = new utilities.genericButton({scene:self,key:'buttonFrame',x:250,y:550,text:"Download"}).setDepth(-1);
         downloadButton.on('pointerdown',function(){
             this.game.photoIndex = carousel.face+1;
@@ -260,8 +247,8 @@ class sceneGallery extends Phaser.Scene
         },self );
            
     }
-    update(){
-        
+    update()
+    {
     }
 
 }
