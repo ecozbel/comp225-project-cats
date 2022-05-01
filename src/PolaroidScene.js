40f95a2209f35783031d81762211cce9e888d1b9
ecozbel <73610579+ecozbel@users.noreply.github.com>
2022-05-01 17:24:06 -0500

    -Organized polaroid scene

--- src/PolaroidScene.js
+++ src/PolaroidScene.js
@@ -1,8 +1,5 @@
 import Phaser from "phaser";
-//import * as imports from "./importHelperE.js"
 import * as utilities from "./utilities.js";
-//import * as utilities from "./utilities.js";
-
 
 var cat;
 var polaroid;
@@ -22,14 +19,13 @@ class PolaroidScene extends Phaser.Scene
 
     preload ()
     {
-
     }
 
     create ()
     {
         pictureCreated = false;
-
         var self = this;
+        //Get correct prompt and put the name of the cat
         function getPromptWithName(prompt,name) {
             return {
                 objective: prompt.objective.replaceAll("{{full_name}}", name),
@@ -41,14 +37,11 @@ class PolaroidScene extends Phaser.Scene
         }
         this.newPrompt = getPromptWithName(this.game.cat.generatedPrompt,this.game.cat.name);
         console.log(this.newPrompt.backgroundType);
+        //Set up for art assets in scene
         this.initialSpriteSetup(self);
-
         var printSound = this.sound.add('printSound',{ loop: false });
-
         var shutterSound = this.sound.add('shutterSound',{ loop: false });
-        //var newPrompt = getPromptWithName(this.game.cat.generatedPrompt,this.game.cat.name);
-
-
+        //Set up camera button
         var cameraButton = this.add.sprite(400,300,'cameraAnim')
         .setDepth(4)
         .setInteractive({ useHandCursor: true });
@@ -57,13 +50,10 @@ class PolaroidScene extends Phaser.Scene
             frames: this.anims.generateFrameNumbers('cameraAnim',{ start: 0, end: 4 }),
             frameRate: 5
         });
-
-        //cameraButton.play({key:'cameraAnim',repeat:-1});
         cameraButton.on('pointerover', () => cameraButton.play({key:'cameraflashing',repeat:-1}));
         cameraButton.on('pointerout', () => cameraButton.stop().setFrame(0));
         this.cameras.main.setBackgroundColor('000000');
-
-        //"Takes photo" when clicked on screen
+        //"Takes photo" when clicked on camera button and produces a polaroid
         cameraButton.on('pointerdown', function () {
             cameraButton.setVisible(false);
             if (pictureCreated == false){
@@ -71,23 +61,16 @@ class PolaroidScene extends Phaser.Scene
                 utilities.playSoundEffect(shutterSound);
                 this.cameras.main.flash(1000);
                 creategalleryButton()
-                //ADD camera flash sound here
                 this.setUpPolaroid();
-                //this.setUpCat(400,-1000);
                 this.setUpTween(cat,polaroid,self);
                 gameReady=true;
-                console.log(cat.texture);
-                
-
                 let shoe = cat.shoePosition.currentClothing;
                 let hat = cat.hatPosition.currentClothing;
                 let shirt = cat.shirtPosition.currentClothing;
-                let pants = cat.pantsPosition.currentClothing;
-
-            
+                let pants = cat.pantsPosition.currentClothing;            
                 var polaroidCount = localStorage.getItem('polaroidCount');
                 console.log(polaroidCount);
-
+                //Find the correct local storage index and save clothing keys
                 if(polaroidCount == null){
                     localStorage.setItem('polaroidCount',"1")
                     let index = 1;
@@ -115,38 +98,23 @@ class PolaroidScene extends Phaser.Scene
                     saveClothing("shirt"+index,shirt);
                     saveClothing("pants"+index,pants);
                     saveClothing("shoes"+index,shoe);
-                    
-
                 }
-
             }
-
-
         }, this);
-
-        this.events.once('slidePolaroid', function (){
-
-            console.log("Sliding polaroid");
-        },this);
-
-
         //Listener for when camera flash effect is done
         this.cameras.main.on('cameraflashcomplete', function () {
-            //Maybe could add camera sound here too
             utilities.playSoundEffect(printSound);
-
         });
-
+        //Save the texture key of current clothing on local storage
         function saveClothing(saveKey,clothing){
-
             if(clothing!=null){
                 localStorage.setItem(saveKey,clothing.texture.key);
             }
             else{
                 localStorage.setItem(saveKey,"empty");
             }
-
         }
+        //Creates a button to go to the gallery scene
         function creategalleryButton(){
             var galleryButton= new utilities.genericButton({scene:self,key:'buttonFrame',x:400,y:580,text:"Go to gallery",displayWidth: 300, displayHeight:36});
             galleryButton.on('pointerdown', function(pointer, localX, localY, event){
@@ -155,8 +123,6 @@ class PolaroidScene extends Phaser.Scene
             galleryButton.setDepth(5);
             galleryButton.currentText.setDepth(6);
         }
-    
-
     }
     update(){
         if (gameReady){
@@ -170,6 +136,7 @@ class PolaroidScene extends Phaser.Scene
         cat.x=0;
         cat.y=0;
         frame = currentScene.add.sprite(0,0,'polaroid');
+        //Add appropriate background connected to prompt
         if(currentScene.newPrompt.backgroundType == "town"){
             bg = currentScene.add.sprite(0,0,'scenery1',);
         }
@@ -182,8 +149,8 @@ class PolaroidScene extends Phaser.Scene
         if(currentScene.newPrompt.backgroundType == "restaurant"){
             bg = currentScene.add.sprite(0,0,'scenery4',);
         }       
+        //Check for any undefined clothing sprites
         if (cat.hatPosition.currentClothing != null && cat.hatPosition.currentClothing.active==true) {
-            console.log(cat.hatPosition.currentClothing);
             currentScene.add.existing(cat.hatPosition.currentClothing);
             cat.hatPosition.currentClothing.setVisible(false);
             let pipelineInstance = currentScene.plugins.get('rexoutlinepipelineplugin').get(cat.hatPosition.currentClothing)[0];
@@ -192,7 +159,6 @@ class PolaroidScene extends Phaser.Scene
             }
         }
         if (cat.shoePosition.currentClothing != null&& cat.shoePosition.currentClothing.active==true) {
-            console.log(cat.shoePosition.currentClothing);
             currentScene.add.existing(cat.shoePosition.currentClothing);
             cat.shoePosition.currentClothing.setVisible(false);
             let pipelineInstance = currentScene.plugins.get('rexoutlinepipelineplugin').get(cat.shoePosition.currentClothing)[0];
@@ -201,7 +167,6 @@ class PolaroidScene extends Phaser.Scene
             }
         }
         if (cat.pantsPosition.currentClothing != null&& cat.pantsPosition.currentClothing.active==true) {
-            console.log(cat.pantsPosition.currentClothing);
             currentScene.add.existing(cat.pantsPosition.currentClothing);
             cat.pantsPosition.currentClothing.setVisible(false);
             let pipelineInstance = currentScene.plugins.get('rexoutlinepipelineplugin').get(cat.pantsPosition.currentClothing)[0];
@@ -210,7 +175,6 @@ class PolaroidScene extends Phaser.Scene
             }
         }
         if (cat.shirtPosition.currentClothing != null&& cat.shirtPosition.currentClothing.active==true) {
-            console.log(cat.shirtPosition.currentClothing);
             currentScene.add.existing(cat.shirtPosition.currentClothing);
             cat.shirtPosition.currentClothing.setVisible(false);
             let pipelineInstance = currentScene.plugins.get('rexoutlinepipelineplugin').get(cat.shirtPosition.currentClothing)[0];
@@ -221,28 +185,6 @@ class PolaroidScene extends Phaser.Scene
         else{
             console.log("Sprite undefined")
         }
-
-
-
-        console.log("cat: ");
-        console.log(cat);
-
-        
-
-        // function getPromptWithName(prompt,name) {
-        //     return {
-        //         objective: prompt.objective.replaceAll("{{full_name}}", name),
-        //         introduction: prompt.introduction.replaceAll("{{full_name}}", name),
-        //         outcome: prompt.outcome.replaceAll("{{full_name}}", name),
-        //         backgroundType : prompt.backgroundType,
-        //         index : prompt.promptIndex
-        //     };
-        // }
-
-        // var newPrompt = getPromptWithName(this.game.cat.generatedPrompt,this.game.cat.name);
-        
-        console.log("after prompt generated")//for debugging
-
         endingPrompt = currentScene.add.text(0,0,currentScene.newPrompt.outcome,{
 			fontFamily: 'Permanent Marker',
 			fontSize: '18px',
@@ -251,35 +193,32 @@ class PolaroidScene extends Phaser.Scene
 			resolution: 1,
             wordWrap : {width : 500, useAdvancedWrap : true},
 		});
-
+        //Set all invisible until polaroid is produced on screen
         cat.setVisible(false);
         frame.setVisible(false);
         bg.setVisible(false);
         endingPrompt.setVisible(false);
-
     }
 
     setUpPolaroid(){
+        //Set up cat
         cat.setVisible(true);
         cat.setDepth(3);
         cat.setScale(3.5);
-
+        //Set up background
         bg.setVisible(true);
         bg.setScale(0.6);
         bg.setDepth(-1);
-
+        //Set up polaroid frame
         frame.setVisible(true);
         frame.setScale(20);
         frame.setDepth(1);
-
+        //Set up written prompt
         endingPrompt.setVisible(true);
-        endingPrompt.set
-        //add all contents of polaroid into container
+        //Add all contents of polaroid into container
         polaroid = this.add.container(400,-1000,[ bg,frame,cat,endingPrompt]);
-
-        console.log("before handleClothingItemPolaroidSLide:");//debugging
+        //Set up animations for clothing items
         if (cat.shoePosition.currentClothing != null) {
-            
             handleClothingItemPolaroidSlide(cat.shoePosition.currentClothing,-21.5,cat.displayHeight/2.61,1,cat);
         }   
         if (cat.pantsPosition.currentClothing != null) {
@@ -291,19 +230,13 @@ class PolaroidScene extends Phaser.Scene
         if (cat.hatPosition.currentClothing != null) {
             handleClothingItemPolaroidSlide(cat.hatPosition.currentClothing,-20,-cat.displayHeight/2.4,4,cat);
         }
-
-
-        console.log("cat boundingBox:");//debugging
-        console.log(cat.boundingBox);
-        //adjusts sprite's position relative to the container
+        //Adjusts sprite's position relative to the container
         bg.y=bg.y-50;
         endingPrompt.y=endingPrompt.y+50;
         endingPrompt.x= frame.x-240;
         endingPrompt.y= frame.y +165;
         cat.x=cat.x-25;
-
-
-        //helper function to reduce code duplication
+        //Helper function to reduce code duplication
         function handleClothingItemPolaroidSlide(clothing,xOffset,yOffset,depth,cat){
             if (clothing != null) {
                 
@@ -315,10 +248,7 @@ class PolaroidScene extends Phaser.Scene
                 clothing.setDepth(depth);//it is function that's causing the crash. don't know why.
             }
         }
-
     }
-    
-
     setUpTween(cat,polaroid,gameScene){
         gameScene.polaroidSlideOver = gameScene.tweens.add({
             targets: polaroid,
@@ -328,28 +258,7 @@ class PolaroidScene extends Phaser.Scene
             yoyo: false,
             delay: 0
         });
-        console.log("built tweens");
-
-        const restartButton = this.add.image(polaroid.x, polaroid.y +polaroid.displayHeight/1.75 , 'itemFrame')
-            .setDisplaySize(300, 50)
-            .setDepth(4)
-            .setInteractive({ useHandCursor: true })
-            //call function to pass on cat and prompt selection to next scene here
-            .on('pointerdown', function(pointer, localX, localY, event){
-                this.scene.start('scene')
-          
-            },self );
-
-        
-            this.add.text(restartButton.x, restartButton.y, 'Restart',{ fontFamily: 'MinecraftiaRegular', fontSize: '18px',stroke: '#000000',strokeThickness: 2,align:'left'  })
-                .setOrigin(0.5)
-                .setDepth(4);
     }
-    
-
-
-
-
 }
 
 
